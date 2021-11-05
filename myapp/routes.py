02897631ea4e1d09300e49d073b3bb863181aa58
Quang Chu <anhquang.chu@sjsu.edu>
2021-11-04 20:46:42 -0700

    added a function that would return rank of city and sort the cities by rank by a given key

--- myapp/routes.py
+++ myapp/routes.py
@@ -4,12 +4,16 @@ from flask import render_template, redirect, flash
 from myapp import db
 from myapp.models import Cities
 
+def byRank(obj):
+    return obj.rank
+
 @myapp_obj.route("/", methods=['GET', 'POST'])
 def home():
     title = "Top Cities"
     name = "Superman"
     form = TopCities()
     top_cities = Cities.query.all()
+    top_cities.sort(key = byRank)
     if form.validate_on_submit():
         city = Cities(name = form.city_name.data, rank= form.city_rank.data, isVisited = form.is_visited.data)
         db.session.add(city)
@@ -17,3 +21,4 @@ def home():
         flash(f'City name "{form.city_name.data}" of rank {form.city_rank.data} visited: {form.is_visited.data} was added')
         return redirect("/")
     return render_template("home.html",form = form, title = title, name = name, top_cities=top_cities)
+
